3ba284ae04297fe477a2a3cf90dda3b66d014878
Jim Carpenter <jim@carpenteronline.us>
2024-03-06 07:15:42 -0500

    slider animation complete

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,13 +2,29 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle, faDotCircle } from "@fortawesome/free-regular-svg-icons";
 
-import galleryImage1 from "../_images/gallery-img-1.jpg";
+import galleryImage1 from "../_images/gallery1.jpg";
+import galleryImage2 from "../_images/gallery2.jpg";
+import galleryImage3 from "../_images/gallery3.jpg";
 
 const Gallery = () => {
   return (
     <>
       <div className='gallery'>
-        <img className='gallery__image' src={galleryImage1} alt='CNC Tooling' />
+        <img
+          className='gallery__image'
+          src={galleryImage1}
+          alt='CNC Tooling Gallery Slide 1'
+        />
+        <img
+          className='gallery__image'
+          src={galleryImage2}
+          alt='CNC Tooling Gallery Slide 2'
+        />
+        <img
+          className='gallery__image'
+          src={galleryImage3}
+          alt='CNC Tooling Gallery Slide 3'
+        />
       </div>
       <div className='gallery-nav'>
         <ul className='gallery-nav__list'>
